64c915ec76208ec320b01388596c67cfee6cef87
Ryuu <ranggawahyudvlpr@gmail.com>
2024-01-19 19:08:47 +0700

    feat: Contact API

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\ContactResource;
+use App\Http\Requests\ContactCreateRequest;
+use App\Http\Requests\ContactUpdateRequest;
+use App\Http\Resources\ContactCollection;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ContactController extends Controller
+{
+    public function create(ContactCreateRequest $requset): JsonResponse
+    {
+        $data = $requset->validated();
+        $user = Auth::user();
+
+        $contact = new Contact($data);
+        $contact->user_id = $user->id;
+        $contact->save();
+
+        return (new ContactResource($contact))->response()->setStatusCode(201);
+    }
+
+    public function get(int $id): ContactResource
+    {
+        $user = Auth::user();
+        $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
+
+        if (!$contact) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    'message' => ['not found']
+                ]
+            ])->setStatusCode(404));
+        }
+
+        return new ContactResource($contact);
+    }
+
+    public function update(int $id, ContactUpdateRequest $requset): ContactResource
+    {
+        $user = Auth::user();
+        $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
+        if (!$contact) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    'message' => ['not found']
+                ]
+            ])->setStatusCode(404));
+        }
+
+        $data = $requset->validated();
+        $contact->fill($data);
+        $contact->save();
+
+        return new ContactResource($contact);
+    }
+
+    public function delete(int $id): JsonResponse
+    {
+        $user = Auth::user();
+        $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
+        if (!$contact) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    'message' => ['not found']
+                ]
+            ])->setStatusCode(404));
+        }
+
+        $contact->delete();
+
+        return response()->json([
+            'data' => true
+        ])->setStatusCode(200);
+    }
+
+    public function search(Request $request): ContactCollection
+    {
+        $user = Auth::user();
+        $page = $request->input('page', 1);
+        $size = $request->input('size', 10);
+
+        $contact = Contact::query()->where('user_id', $user->id);
+
+        $contact = $contact->where(function (Builder $builder) use ($request) {
+            $name = $request->input('name');
+            if ($name) {
+                $builder->where(function (Builder $builder) use ($name) {
+                    $builder->orWhere('first_name', 'like', '%' . $name . '%');
+                    $builder->orWhere('last_name', 'like', '%' . $name . '%');
+                });
+            }
+
+            $email = $request->input('email');
+            if ($email) {
+                $builder->Where('email', 'like', '%' . $email . '%');
+            }
+
+            $phone = $request->input('phone');
+            if ($phone) {
+                $builder->where('phone', 'like', '%' . $phone . '%');
+            }
+        });
+
+        $contact = $contact->paginate(perPage: $size, page: $page);
+
+        return new ContactCollection($contact);
+    }
+}
